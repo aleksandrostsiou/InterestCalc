07d3ff5459c3c3a5f9b61a0638c14e818add411f
aleksandrostsiou <alextsiou13@gmail.com>
2019-09-21 23:47:08 +0300

    Remaned databases.txt to Backup Impelement an input console UI

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MyClassLibrary;
 
@@ -13,9 +14,41 @@ namespace ConsoleApp
         {
             //Passing List<Data> to ctor of Calculator
             Calculator calc = new Calculator(DataManager.GetData());
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = startDate;
 
-            //Input Amount , START date , END date 
-            calc.CalcInterest(100,"01/01/2000", DateTime.Now.ToString("dd/MM/yyyy"));
+            //Console Interface
+            Console.WriteLine("Give the amount ,starting date ,and the ending date \n **in this order**");
+            if (float.TryParse(Console.ReadLine(), out float amount))
+            {
+                Console.WriteLine(" Correct. \n Now give a starting date\n **dd/MM/yyyy**");
+                if (DateTime.TryParse(Console.ReadLine(),out startDate))
+                {
+                    Console.WriteLine(" Congrats!\n At last give me an ending date\n **dd/MM/yyyy**");
+                    if (DateTime.TryParse(Console.ReadLine(), out endDate))
+                    {
+                        Console.WriteLine("Perfect give me just a second");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("Your results : \n");
+
+                        //Input Amount , START date , END date 
+                        calc.CalcInterest(100, startDate.ToString("dd/MM/yyyy"), endDate.ToString("dd/MM/yyyy"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong format.Please try again");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong format.Please try again");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I told u with this order");
+            }
+            
         }
     }
 }
